78710b0ca77171f60e867596806a4071758da7db
david-amplified <david.a.lavely@gmail.com>
2022-06-10 21:40:40 -0600

    Product Search Page completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,15 +10,20 @@ class ProductController extends Controller
     //
     function index()
     {
-        $data= Product::all();
+        $data = Product::all();
 
         return view('product',['products'=>$data]);
     }
 
     function detail($id)
     {
-        $data =Product::find($id);
+        $data = Product::find($id);
         return view('detail',['product'=>$data]);
     }
 
+    function search(Request $req)
+    {
+        $data = Product::where('name','LIKE', '%'. $req->input('query'). '%')->get();
+        return view('search',['products'=>$data]);
+    }
 }
